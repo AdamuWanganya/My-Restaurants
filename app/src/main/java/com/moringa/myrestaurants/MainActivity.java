125d8f6e7717c045836446907c006b2de3cbc03b
AdamuWanganya <wanganyaadamu@gmail.com>
2022-02-05 03:21:01 +0300

    Initial commit

---
+++ app/src/main/java/com/moringa/myrestaurants/MainActivity.java
@@ -0,0 +1,45 @@
+package com.moringa.myrestaurants;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.appNameTextView) TextView mappNameTextView;
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = mLocationEditText.getText().toString();
+//                Log.d(TAG, location);
+                Toast.makeText(MainActivity.this,location,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
